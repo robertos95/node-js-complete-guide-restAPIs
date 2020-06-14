284be71531a804049b271b905c3d22798d356604
Roberto Salim <salimroberto95@gmail.com>
2020-06-14 18:23:18 +0800

    end of 4.11. Input Validation.mp4 (joi)

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,5 +1,8 @@
+const Joi = require("joi");
 const express = require("express");
 const app = express();
+
+app.use(express.json());
 
 const courses = [
   { id: 1, name: "course1" },
@@ -17,7 +20,25 @@ app.get("/api/courses", (req, res) => {
 
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if(!course) res.status(404).send("The course with the given ID was not found");
+  if (!course)
+    res.status(404).send("The course with the given ID was not found");
+  res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  const schema = { name: Joi.string().min(3).required() };
+
+  const result = Joi.validate(req.body, schema);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
   res.send(course);
 });
 
