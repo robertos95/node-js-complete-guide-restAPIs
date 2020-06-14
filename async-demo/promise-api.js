2f1341734cadcaba4612a71ddcabe16ccec7292b
Roberto Salim <salimroberto95@gmail.com>
2020-06-14 22:52:52 +0800

    end of 6.10. Running Parallel Promises.mp4

--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -1,5 +1,27 @@
-const p = Promise.resolve({ id: 1 });
-p.then((result) => console.log(result));
+// const p = Promise.resolve({ id: 1 });
+// p.then((result) => console.log(result));
 
-const p2 = Promise.reject(new Error('Reason for rejection'));
-p2.catch(err => console.log(err.message))
+// const p2 = Promise.reject(new Error('Reason for rejection'));
+// p2.catch(err => console.log(err.message))
+
+const p1 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("Async operation 1");
+    resolve(1);
+  }, 3000);
+});
+
+const p2 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("Async operation 2");
+    resolve(2);
+  }, 2000);
+});
+
+Promise.all([p1, p2])
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err.message));
+
+Promise.race([p1, p2])
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err.message));
