bca6a3830b4c0a74b9e12397b9880b9ae9f139a8
Roberto Salim <salimroberto95@gmail.com>
2020-06-14 22:45:33 +0800

    end of 6.9. Creating Settled Promises.mp4

---
+++ async-demo/promise-api.js
@@ -0,0 +1,5 @@
+const p = Promise.resolve({ id: 1 });
+p.then((result) => console.log(result));
+
+const p2 = Promise.reject(new Error('Reason for rejection'));
+p2.catch(err => console.log(err.message))
