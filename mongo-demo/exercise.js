c972c21a8dafb76f4ac5e73664f9e30869fea3be
Roberto Salim <salimroberto95@gmail.com>
2020-06-15 01:00:45 +0800

    end of 7.14. Exercise 1.mp4

---
+++ mongo-demo/exercise.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/mongo-exercises")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.log("Could not connect to MongoDB...", err));
+
+const courseSchema = new mongoose.Schema({
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  name: String,
+  author: String,
+  isPublished: Boolean,
+  price: Number,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+async function getCourses() {
+  return await Course.find({ isPublished: true, tags: "backend" })
+    .sort({ name: 1 })
+    .select({ name: 1, author: 1 });
+}
+
+async function run() {
+  const courses = await getCourses();
+  console.log(courses);
+}
+run();
