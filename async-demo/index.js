ac9e151caa3b8bc0c46a1b4fbd6c60ae15b52141
Roberto Salim <salimroberto95@gmail.com>
2020-06-14 21:44:02 +0800

    end of 6.3. Callbacks.mp4

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,7 +1,22 @@
 console.log("Before");
-
-setTimeout(() => {
-  console.log("Reading a user from the database...");
-}, 2000);
-
+getUser(1, (user) => {
+  console.log("User", user);
+  getRepositories(user.gitHubUsername, (repos) => {
+    console.log("Repos", repos);
+  });
+});
 console.log("After");
+
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log("Reading a user from the database...");
+    callback({ id: id, gitHubUsername: "robertos95" });
+  }, 2000);
+}
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log("Calling Github API...");
+    callback(["repo1", "repo2", "repo3"]);
+  }, 2000);
+}
