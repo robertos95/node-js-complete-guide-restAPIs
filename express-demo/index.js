256a9938de3539f55895db35d75db6402280561b
Roberto Salim <salimroberto95@gmail.com>
2020-06-14 17:56:36 +0800

    end of 4.7. Route Parameters.mp4

--- express-demo/index.js
+++ express-demo/index.js
@@ -9,5 +9,10 @@ app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3]);
 });
 
+app.get("/api/posts/:year/:month", (req, res) => {
+  res.send(req.params);
+  // res.send(req.query);
+})
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
