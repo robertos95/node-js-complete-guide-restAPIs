0167105ca0dfb1d6ad66c7edb84f1be9079fc78c
Roberto Salim <salimroberto95@gmail.com>
2020-06-16 03:52:22 +0800

    end of 11.11. Refactoring Index.js- Extracting Routes.mp4

--- vidly/index.js
+++ vidly/index.js
@@ -1,30 +1,16 @@
 require("express-async-errors");
 const winston = require("winston");
 require("winston-mongodb");
-const error = require("./middleware/error");
 const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
 const express = require("express");
 const app = express();
 
-// UNCAUGHT EXC/UNHANDLED REJ METHOD 1
-// process.on("uncaughtException", (ex) => {
-//   winston.error(ex.message, ex);
-// });
+require("./startup/routes")(app);
 
-// process.on("unhandledRejection", (ex) => {
-//   winston.error(ex.message, ex);
-// });
-
-// RECOMMENDED - UNCAUGHT EXC/UNHANDLED REJ METHOD 2
+// RECOMMENDED - UNCAUGHT EXC/UNHANDLED REJ
 winston.handleExceptions(
   new winston.transports.File({ filename: "uncaughtExceptions.log" })
 );
@@ -41,10 +27,6 @@ winston.add(
   })
 );
 
-// throw new Error("Something failed during startup.");
-const p = Promise.reject(new Error("Somethign failed miserably."));
-p.then(() => console.log("Done"));
-
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined.");
   process.exit(1);
@@ -54,16 +36,6 @@ mongoose
   .connect("mongodb://localhost/vidly")
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB..."));
-
-app.use(express.json());
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
