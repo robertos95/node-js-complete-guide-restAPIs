070549b5a560db9ea715754df2824997e90ba87c
Roberto Salim <salimroberto95@gmail.com>
2020-06-16 03:26:48 +0800

    end of 11.7. Logging to MongoDB.mp4

--- vidly/index.js
+++ vidly/index.js
@@ -1,5 +1,6 @@
 require("express-async-errors");
 const winston = require("winston");
+require("winston-mongodb");
 const error = require("./middleware/error");
 const config = require("config");
 const Joi = require("joi");
@@ -15,6 +16,12 @@ const express = require("express");
 const app = express();
 
 winston.add(new winston.transports.File({ filename: "logfile.log" }));
+winston.add(
+  new winston.transports.MongoDB({
+    db: "mongodb://localhost/vidly",
+    level: "info",
+  })
+);
 
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined.");
