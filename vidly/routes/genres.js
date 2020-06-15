098ec5713c63ec36a5d68374dc2caaa0b65803f7
Roberto Salim <salimroberto95@gmail.com>
2020-06-16 02:48:20 +0800

    end of 10.17. Role Based Authorization.mp4

--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -1,4 +1,5 @@
 const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
 const { Genre, validate } = require("../models/genre");
 const mongoose = require("mongoose");
 const express = require("express");
@@ -37,7 +38,7 @@ router.put("/:id", async (req, res) => {
   res.send(genre);
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", [auth, admin], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
 
   if (!genre)
