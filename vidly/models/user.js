242079c08a74bed9ae0521a81dccc6a0e7b8ee47
Roberto Salim <salimroberto95@gmail.com>
2020-06-15 23:25:50 +0800

    end of 10.3. Registering Users.mp4

--- vidly/models/user.js
+++ vidly/models/user.js
@@ -28,8 +28,8 @@ const User = mongoose.model("User", userSchema);
 function validateUser(user) {
   const schema = {
     name: Joi.string().min(5).max(50).required(),
-    type: Joi.string().min(5).max(255).required().email(),
-    name: Joi.string().min(5).max(255).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
   };
 
   return Joi.validate(user, schema);
