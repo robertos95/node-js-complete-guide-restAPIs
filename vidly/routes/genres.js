344d8366b2266940c48bc90da237cd6b34ec3128
Roberto Salim <salimroberto95@gmail.com>
2020-06-16 03:01:41 +0800

    end of 11.4. Removing Try_Catch docs.mp4

--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -1,3 +1,4 @@
+const asyncMiddleware = require("../middleware/async");
 const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
 const { Genre, validate } = require("../models/genre");
@@ -14,33 +15,40 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.post("/", auth, async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+router.post(
+  "/",
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-  let genre = new Genre({ name: req.body.name });
-  await genre.save();
+    let genre = new Genre({ name: req.body.name });
+    await genre.save();
 
-  res.send(genre);
-});
+    res.send(genre);
+  })
+);
 
-router.put("/:id", async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+router.put(
+  "/:id",
+  asyncMiddleware(async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-  const genre = await Genre.findByIdAndUpdate(
-    req.params.id,
-    { name: req.body.name },
-    {
-      new: true,
-    }
-  );
+    const genre = await Genre.findByIdAndUpdate(
+      req.params.id,
+      { name: req.body.name },
+      {
+        new: true,
+      }
+    );
 
-  if (!genre)
-    return res.status(404).send("The genre with the given ID was not found.");
+    if (!genre)
+      return res.status(404).send("The genre with the given ID was not found.");
 
-  res.send(genre);
-});
+    res.send(genre);
+  })
+);
 
 router.delete("/:id", [auth, admin], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
