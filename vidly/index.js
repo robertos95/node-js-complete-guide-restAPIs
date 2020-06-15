9bd5c283b2b3bb1b6d42a2a22f96895006b01ed7
Roberto Salim <salimroberto95@gmail.com>
2020-06-16 04:04:20 +0800

    end of 11.15. Extracting the Validation Logic.mp4

--- vidly/index.js
+++ vidly/index.js
@@ -1,36 +1,12 @@
-require("express-async-errors");
 const winston = require("winston");
-require("winston-mongodb");
-const config = require("config");
-const Joi = require("joi");
-Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
 const app = express();
 
+require("./startup/logging")();
 require("./startup/routes")(app);
-require("./startup/db");
-
-// RECOMMENDED - UNCAUGHT EXC/UNHANDLED REJ
-winston.handleExceptions(
-  new winston.transports.File({ filename: "uncaughtExceptions.log" })
-);
-
-process.on("unhandledRejection", (ex) => {
-  throw ex;
-});
-
-winston.add(new winston.transports.File({ filename: "logfile.log" }));
-winston.add(
-  new winston.transports.MongoDB({
-    db: "mongodb://localhost/vidly",
-    level: "info",
-  })
-);
-
-if (!config.get("jwtPrivateKey")) {
-  console.error("FATAL ERROR: jwtPrivateKey is not defined.");
-  process.exit(1);
-}
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/validation")();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => winston.info(`Listening on port ${port}...`));
---
+++ vidly/startup/logging.js
@@ -0,0 +1,22 @@
+require("express-async-errors");
+const winston = require("winston");
+require("winston-mongodb");
+
+module.exports = function () {
+  // RECOMMENDED - UNCAUGHT EXC/UNHANDLED REJ
+  winston.exceptions.handle(
+    new winston.transports.File({ filename: "uncaughtExceptions.log" })
+  );
+
+  process.on("unhandledRejection", (ex) => {
+    throw ex;
+  });
+
+  winston.add(new winston.transports.File({ filename: "logfile.log" }));
+  winston.add(
+    new winston.transports.MongoDB({
+      db: "mongodb://localhost/vidly",
+      level: "info",
+    })
+  );
+};
