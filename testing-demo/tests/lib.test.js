a0515c4aa2e01cbaa1c1aca99952e323543bd58b
Roberto Salim <salimroberto95@gmail.com>
2020-06-16 05:38:58 +0800

    end of 12.17. Interaction Testing.mp4

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,5 +1,6 @@
 const lib = require("../lib");
 const db = require("../db");
+const mail = require("../mail");
 
 describe("absolute", () => {
   it("should return a + number if input is +", () => {
@@ -75,5 +76,21 @@ describe("applyDiscount", () => {
     const order = { customerId: 1, totalPrice: 10 };
     lib.applyDiscount(order);
     expect(order.totalPrice).toBe(9);
+  });
+});
+
+describe("notifyCustomer", () => {
+  it("should send an email to the customer", () => {
+    db.getCustomerSync = function (customerId) {
+      return { email: "a" };
+    };
+
+    let mailSent = false;
+    mail.send = function (email, message) {
+      mailSent = true;
+    };
+
+    lib.notifyCustomer({ customerId: 1 });
+    expect(mailSent).toBe(true);
   });
 });
