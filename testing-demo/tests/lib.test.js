fdc26bcdede7e3286f6e912ac7b68a100d461f5a
Roberto Salim <salimroberto95@gmail.com>
2020-06-16 05:47:28 +0800

    end of 12.18. Jest Mock Functions.mp4

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -81,16 +81,13 @@ describe("applyDiscount", () => {
 
 describe("notifyCustomer", () => {
   it("should send an email to the customer", () => {
-    db.getCustomerSync = function (customerId) {
-      return { email: "a" };
-    };
-
-    let mailSent = false;
-    mail.send = function (email, message) {
-      mailSent = true;
-    };
+    db.getCustomerSync = jest.fn().mockReturnValue({ email: "a" });
+    mail.send = jest.fn();
 
     lib.notifyCustomer({ customerId: 1 });
-    expect(mailSent).toBe(true);
+
+    expect(mail.send).toHaveBeenCalled();
+    expect(mail.send.mock.calls[0][0]).toBe("a");
+    expect(mail.send.mock.calls[0][1]).toMatch(/order/);
   });
 });
