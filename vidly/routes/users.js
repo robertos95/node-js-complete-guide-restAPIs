ab1ced5384214effcfe1ca4b37e4a0fbd9285008
Roberto Salim <salimroberto95@gmail.com>
2020-06-16 02:00:36 +0800

    end of 10.11. Setting Response Headers.mp4

--- vidly/routes/users.js
+++ vidly/routes/users.js
@@ -1,3 +1,5 @@
+const config = require("config");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const _ = require("lodash");
 const { User, validate } = require("../models/user");
@@ -17,7 +19,10 @@ router.post("/", async (req, res) => {
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
-  res.send(_.pick(user, ["name", "email"]));
+  const token = jwt.sign({ _id: user._id }, config.get("jwtPrivateKey"));
+  res
+    .header("x-auth-token", token)
+    .send(_.pick(user, ["_id", "name", "email"]));
 });
 
 module.exports = router;
