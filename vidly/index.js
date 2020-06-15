0598678394915bfaf23998f61e208a2418f670fa
Roberto Salim <salimroberto95@gmail.com>
2020-06-16 03:54:39 +0800

    end of 11.12. Extracting the DB Logic.mp4

--- vidly/index.js
+++ vidly/index.js
@@ -4,11 +4,11 @@ require("winston-mongodb");
 const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
-const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 
 require("./startup/routes")(app);
+require("./startup/db");
 
 // RECOMMENDED - UNCAUGHT EXC/UNHANDLED REJ
 winston.handleExceptions(
@@ -31,11 +31,6 @@ if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined.");
   process.exit(1);
 }
-
-mongoose
-  .connect("mongodb://localhost/vidly")
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error("Could not connect to MongoDB..."));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
