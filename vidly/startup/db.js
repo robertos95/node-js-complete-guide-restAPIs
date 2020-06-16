a7af46c0a054c62a5b6ca5b16a9c561fffd7f449
Roberto Salim <salimroberto95@gmail.com>
2020-06-16 17:21:20 +0800

    end of 13.5. Populating the Test DB.mp4

--- vidly/startup/db.js
+++ vidly/startup/db.js
@@ -4,5 +4,11 @@ const config = require("config");
 
 module.exports = function () {
   const db = config.get("db");
-  mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
+  mongoose
+    .connect(db, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => winston.info(`Connected to ${db}...`));
 };
--- vidly/tests/integration/genres.test.js
+++ vidly/tests/integration/genres.test.js
@@ -1,4 +1,5 @@
 const request = require("supertest");
+const { Genre } = require("../../models/genre");
 
 let server;
 
@@ -6,14 +7,24 @@ describe("/api/genres", () => {
   beforeEach(() => {
     server = require("../../index");
   });
-  afterEach(() => {
+  afterEach(async () => {
     server.close();
+    await Genre.deleteMany({});
   });
 
   describe("GET /", () => {
     it("should return all genres", async () => {
+      await Genre.collection.insertMany([
+        { name: "genre1" },
+        { name: "genre2" },
+      ]);
+
       const res = await request(server).get("/api/genres");
+
       expect(res.status).toBe(200);
+      expect(res.body.length).toBe(2);
+      expect(res.body.some((g) => g.name === "genre1")).toBeTruthy();
+      expect(res.body.some((g) => g.name === "genre2")).toBeTruthy();
     });
   });
 });
