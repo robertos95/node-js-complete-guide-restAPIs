2814d91b2b057a5734965f728a62db3861083a9f
Roberto Salim <salimroberto95@gmail.com>
2020-06-16 17:53:40 +0800

    end of 13.10. Testing Invalid Inputs.mp4

--- vidly/tests/integration/genres.test.js
+++ vidly/tests/integration/genres.test.js
@@ -1,5 +1,6 @@
 const request = require("supertest");
 const { Genre } = require("../../models/genre");
+const { User } = require("../../models/user");
 
 let server;
 
@@ -48,8 +49,29 @@ describe("/api/genres", () => {
 
   describe("POST /", () => {
     it("should return 401 if client is not logged in", async () => {
-      const res = await request(server).post("/api/genres").send({ name: "genre1" });
+      const res = await request(server)
+        .post("/api/genres")
+        .send({ name: "genre1" });
       expect(res.status).toBe(401);
+    });
+
+    it("should return 400 if genre is invalid (less than 5 chars)", async () => {
+      const token = new User().generateAuthToken();
+      const res = await request(server)
+        .post("/api/genres")
+        .set("x-auth-token", token)
+        .send({ name: "1234" });
+      expect(res.status).toBe(400);
+    });
+
+    it("should return 400 if genre is invalid (more than 5 chars)", async () => {
+      const token = new User().generateAuthToken();
+      const name = new Array(52).join("a");
+      const res = await request(server)
+        .post("/api/genres")
+        .set("x-auth-token", token)
+        .send({ name: name });
+      expect(res.status).toBe(400);
     });
   });
 });
