d2eabbc301a77b1ee200ae38979e3de9acc54f15
Roberto Salim <salimroberto95@gmail.com>
2020-06-16 22:05:47 +0800

    end of 14.15. Refactoring the Validation Logic.mp4

--- vidly/routes/returns.js
+++ vidly/routes/returns.js
@@ -1,15 +1,13 @@
+const Joi = require("joi");
 const express = require("express");
 const router = express.Router();
 const { Movie } = require("../models/movie");
 const { Rental } = require("../models/rental");
 const auth = require("../middleware/auth");
+const validate = require("../middleware/validate");
 const moment = require("moment");
 
-router.post("/", auth, async (req, res) => {
-  if (!req.body.customerId)
-    return res.status(400).send("customerId not provided");
-  if (!req.body.movieId) return res.status(400).send("movieId not provided");
-
+router.post("/", [auth, validate(validateReturn)], async (req, res) => {
   const rental = await Rental.findOne({
     "customer._id": req.body.customerId,
     "movie._id": req.body.movieId,
@@ -26,7 +24,6 @@ router.post("/", auth, async (req, res) => {
 
   await rental.save();
 
-  
   await Movie.update(
     { _id: rental.movie._id },
     {
@@ -36,5 +33,14 @@ router.post("/", auth, async (req, res) => {
 
   return res.status(200).send(rental);
 });
+
+function validateReturn(req) {
+  const schema = {
+    customerId: Joi.objectId().required(),
+    movieId: Joi.objectId().required(),
+  };
+
+  return Joi.validate(req, schema);
+}
 
 module.exports = router;
